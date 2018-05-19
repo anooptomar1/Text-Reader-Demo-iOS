c38832f8784f85ebc33dab79016c83a99236fdbd
theodormarcu <tmarcu@princeton.edu>
2018-05-19 15:43:36 -0400

    First commit

---
+++ Pods/Target Support Files/Pods-text-recognition/Pods-text-recognition-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_text_recognition : NSObject
+@end
+@implementation PodsDummy_Pods_text_recognition
+@end
---
+++ README.md
@@ -0,0 +1,1 @@
+# Text-Reader-Demo-iOS
